fc79d648102df46c602d608589e70a24c893c9f9
Raj Negi <rajpremsinghnegi@gmail.com>
2016-06-07 04:43:20 +0530

    Completed JS 4th assignment (Hello Username)

---
+++ JS/4 - Hello Username/hello_username.js
@@ -0,0 +1,6 @@
+document.write("4. Hello Username <br><br>");
+
+var username = prompt("Please enter your username: ");
+
+alert("Hello, " + username + "!");
+document.write("<h1>Hello, " + username + "!</h1>");
